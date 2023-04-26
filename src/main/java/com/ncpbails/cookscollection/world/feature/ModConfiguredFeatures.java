730b00bdc453c2baf8dd8de4226997cc15a72e65
Nyancatpig <43119979+Nyancatpig@users.noreply.github.com>
2023-04-27 09:07:09 +0930

    Worked on Lemons

--- src/main/java/com/ncpbails/cookscollection/world/feature/ModConfiguredFeatures.java
+++ src/main/java/com/ncpbails/cookscollection/world/feature/ModConfiguredFeatures.java
@@ -3,10 +3,15 @@ package com.ncpbails.cookscollection.world.feature;
 import com.ncpbails.cookscollection.CooksCollection;
 import com.ncpbails.cookscollection.block.ModBlocks;
 import com.ncpbails.cookscollection.block.custom.FruitingLeaves;
+import com.ncpbails.cookscollection.world.feature.tree.LemonTrunkPlacer;
+import net.minecraft.core.Holder;
 import net.minecraft.core.Registry;
+import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.util.random.SimpleWeightedRandomList;
 import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.util.valueproviders.UniformInt;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
@@ -17,8 +22,11 @@ import net.minecraft.world.level.levelgen.feature.configurations.SimpleBlockConf
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.AcaciaFoliagePlacer;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
 import net.minecraft.world.level.levelgen.feature.stateproviders.WeightedStateProvider;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.BendingTrunkPlacer;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.ForkingTrunkPlacer;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -34,7 +42,7 @@ public class ModConfiguredFeatures {
             CONFIGURED_FEATURES.register("lemon", () ->
                     new ConfiguredFeature<>(Feature.TREE, new TreeConfiguration.TreeConfigurationBuilder(
                             BlockStateProvider.simple(ModBlocks.LEMON_LOG.get()),
-                            new StraightTrunkPlacer(3, 2, 0),
+                            new LemonTrunkPlacer(5, 2, 2),
                             new WeightedStateProvider(SimpleWeightedRandomList.<BlockState>builder().add(ModBlocks.LEMON_LEAVES.get().defaultBlockState(),
                                     3).add(ModBlocks.FRUITING_LEMON_LEAVES.get().defaultBlockState().setValue(FruitingLeaves.AGE, 4), 1)),
                             new AcaciaFoliagePlacer(ConstantInt.of(3), ConstantInt.of(0)),
